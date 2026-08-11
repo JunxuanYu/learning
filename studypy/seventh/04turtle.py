748f8ccd33e328f4f84560a83d65874838633d75
hid26074282 <2157926389@qq.com>
2026-08-11 16:38:52 +0800

    seventh file

---
+++ studypy/seventh/04turtle.py
@@ -0,0 +1,13 @@
+"""
+使用turtle库的turtle.right()函数和turtle.fd()函数绘制一个菱形，边长为200像素，4个内角度数为2个60度和2个120度，
+"""
+import turtle
+turtle.right(-30)
+turtle.fd(200)
+turtle.right(60)
+turtle.fd(200)
+turtle.right(120)
+turtle.fd(200)
+turtle.right(60)
+turtle.fd(200)
+turtle.right(120)
